932b5588298b4ce13aa59a171fec55d6498fb9a8
rdv0011 <rdv0011@gmail.com>
2026-05-17 16:39:36 +0200

    Use KMM gradle plugin to generate xcode framework

--- feature-restaurant/src/iosMain/kotlin/io/umain/munchies/feature/restaurant/di/FeatureRestaurantIos.kt
+++ feature-restaurant/src/iosMain/kotlin/io/umain/munchies/feature/restaurant/di/FeatureRestaurantIos.kt
@@ -3,10 +3,16 @@ package io.umain.munchies.feature.restaurant.di
 import io.umain.munchies.di.getKoin
 import io.umain.munchies.feature.restaurant.navigation.ios.RestaurantDetailRouteHandlerImpl
 import io.umain.munchies.feature.restaurant.navigation.ios.RestaurantListRouteHandlerImpl
+import io.umain.munchies.feature.restaurant.presentation.RestaurantListViewModel
+import io.umain.munchies.feature.restaurant.presentation.RestaurantDetailViewModel
+import io.umain.munchies.feature.restaurant.navigation.RestaurantNavigationViewModel
+import io.umain.munchies.feature.restaurant.presentation.state.RestaurantListUiState
+import io.umain.munchies.feature.restaurant.presentation.state.RestaurantDetailUiState
 import io.umain.munchies.navigation.RestaurantDetailRoute
 import io.umain.munchies.navigation.RestaurantListRoute
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
+import org.koin.core.parameter.parametersOf
 
 /**
  * iOS helpers to retrieve shared ViewModel instances from Koin.
@@ -65,3 +71,33 @@ fun createRestaurantDetailRoute(restaurantId: String): RestaurantDetailRoute =
 fun getRestaurantListRouteHandler() = RestaurantListRouteHandlerImpl
 
 fun getRestaurantDetailRouteHandler() = RestaurantDetailRouteHandlerImpl
+
+// ViewModel export functions (for framework public API)
+/**
+ * Get the RestaurantListViewModel from the scope.
+ * Must be called after the scope is created via createRestaurantListScopeIos().
+ */
+fun getRestaurantListViewModelIos(): RestaurantListViewModel {
+    val koin = getKoin()
+    val scope = koin.getScope(RestaurantListScope.value)
+    return scope.get<RestaurantListViewModel>()
+}
+
+/**
+ * Get the RestaurantDetailViewModel from the scope.
+ * Must be called after the scope is created via createRestaurantDetailScopeIos(restaurantId).
+ */
+fun getRestaurantDetailViewModelIos(restaurantId: String): RestaurantDetailViewModel {
+    val koin = getKoin()
+    val scopeId = RestaurantDetailScope(restaurantId).value
+    val scope = koin.getScope(scopeId)
+    return scope.get<RestaurantDetailViewModel> { parametersOf(restaurantId) }
+}
+
+/**
+ * Get the RestaurantNavigationViewModel for restaurant feature.
+ */
+fun getRestaurantNavigationViewModelIos(): RestaurantNavigationViewModel {
+    val koin = getKoin()
+    return koin.get<RestaurantNavigationViewModel>()
+}
--- feature-settings/src/iosMain/kotlin/io/umain/munchies/feature/settings/di/FeatureSettingsIos.kt
+++ feature-settings/src/iosMain/kotlin/io/umain/munchies/feature/settings/di/FeatureSettingsIos.kt
@@ -2,6 +2,7 @@ package io.umain.munchies.feature.settings.di
 
 import io.umain.munchies.di.getKoin
 import io.umain.munchies.feature.settings.navigation.ios.SettingsRouteHandlerImpl
+import io.umain.munchies.feature.settings.presentation.SettingsViewModel
 import io.umain.munchies.navigation.SettingsRoute
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
@@ -21,3 +22,13 @@ fun createSettingsRoute(): SettingsRoute = SettingsRoute()
 
 fun getSettingsRouteHandler() = SettingsRouteHandlerImpl
 
+// ViewModel export functions (for framework public API)
+/**
+ * Get the SettingsViewModel from the scope.
+ * Must be called after the scope is created via createSettingsScopeIos().
+ */
+fun getSettingsViewModelIos(): SettingsViewModel {
+    val koin = getKoin()
+    val scope = koin.getScope(SettingsScope.value)
+    return scope.get<SettingsViewModel>()
+}
